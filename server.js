58604217e5d23ae546cc86cba521b5d92ffd3f2e
Satayu037 <664259037@webmail.npru.ac.th>
2026-06-25 17:02:49 +0700

    add server.js and create express

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
-import { createServer } from "http";
-const server = createServer((req, res) => {
-  const { method, url } = req;
-  res.setHeader("Content-Type", "application/json");
-  if (method === "GET" && url === "/") {
-    res.writeHead(200);
-    res.end(
-      JSON.stringify({
-        message: "สวัสดีจาก Node.js Server!",
-      }),
-    );
-  } else {
-    res.writeHead(404);
-    res.end(JSON.stringify({ error: "ไม่พบหน้าที่ต้องการ" }));
-  }
+import express from 'express';
+import "dotenv/config" ;
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json()); // Middleware: แปลง JSON
+// Routes
+app.get('/', (req, res) => {
+res.json({ message: "สวัสดีจาก Express.js!" });
 });
-server.listen(3000, () => {
-  console.log("Server รันอยูที่ http://localhost:3000");
+
+app.listen(PORT,() => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
